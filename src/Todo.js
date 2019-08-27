d88281f58fba83766a9fc3150075aba99ad85880
Salazar <hector.salazar@itexico.com>
2019-08-27 12:03:12 -0500

    Adds initial state

--- src/Todo.js
+++ src/Todo.js
@@ -9,10 +9,23 @@ const Todo = (props) => {
   }
 
   return (
-    <li
-      onClick={handleClick}
-      className={"br2 ph3 pv2 mb2 " + (completed ? 'strike bg-near-white silver' : 'bg-light-gray near-black') }>
-      <p>{props.task.text}</p>
+    <li className="flex content-stretch">
+      <div
+        className={"br2 ph3 pv2 mb2 " + (completed ? 'bg-near-white' : 'bg-light-gray')}
+        style={{flexGrow: 2}}
+        onClick={handleClick}>
+        <input
+          className={"pa2 bn " + (completed ? 'bg-near-white silver strike' : 'bg-light-gray near-black')}
+          value={props.task.text}
+          //disabled={ !completed }
+          //readOnly={completed}
+          style={{outline: 0}}
+          onChange={() =>{}} />
+      </div>
+      <div
+        className="br2 pa3 pointer mb2 ml2 dim bg-near-white red">
+        Del
+      </div>
     </li>
   )
 }
--- src/TodoList.js
+++ src/TodoList.js
@@ -1,15 +1,22 @@
 import React from 'react'
-
 import Todo from './Todo'
 
-export default class TodoList extends React.Component {
-  render() {
-    return(
-      <div className="pa4-l pa3">
-        <ul className="list pl0 ml0 mw7 b--light-silver br2 center" >
-          {this.props.tasks.map((task, index) => <Todo task={task} key={index} /> )}
-        </ul>
+const TodoList = (props) => {
+  return(
+    <div className="pa4-l pa3">
+      <div className="mw7 center">
+        <button 
+          onClick={props.clearTasks}
+          style={{outline: 0}}
+          className="bn pa1 red dim">
+          Clear Tasks
+        </button>
       </div>
-    )
-  }
+      <ul className="list pl0 ml0 mw7 b--light-silver br2 center" >
+        {props.tasks.map((task, index) => <Todo task={task} key={index} />)}
+      </ul>
+    </div>
+  )
 }
+
+export default TodoList
